f5a791dd74802ca9b993b298274fcf0b5035424a
Rahil Parikh <rparikh@bridge12.com>
2026-02-11 11:10:36 -0500

    fix: Remove missing @testing-library/jest-dom import from setupTests
    - @testing-library/jest-dom is not installed in this project
    - Removing the import that was causing test failures
    - Keeping GSAP mocks which are the essential fix

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,9 +1,3 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import "@testing-library/jest-dom/extend-expect";
-
 // Mock GSAP to prevent import errors in tests
 jest.mock("gsap", () => ({
   gsap: {
